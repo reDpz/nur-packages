d26533244a9fece06686682c5ae188adb3c91e2f
reDpz <sv_redpz@outlook.com>
2025-01-30 19:53:01 +0000

    currently crashing

--- pkgs/frame0/default.nix
+++ pkgs/frame0/default.nix
@@ -1,9 +1,58 @@
-{ stdenv }:
+{ stdenv
+, fetchurl
+, binutils
+, libarchive
+, gtk3
+, nss
+, libnotify
+, libXtst
+, xdg-utils
+, at-spi2-core
+, mesa
+, libdrm
+, libxcb
+, glib
+, alsa-lib
+, libgcc
+, autoPatchelfHook
+}:
 
 stdenv.mkDerivation rec {
-  name = "example-package-${version}";
-  version = "1.0";
-  src = ./.;
-  buildPhase = "echo echo Hello World > example";
-  installPhase = "install -Dm755 example $out";
+  name = "frame0";
+  version = "1.0.0~beta.8";
+  src = fetchurl {
+    url = "https://files.frame0.app/releases/linux/x64/frame0_${version}_amd64.deb";
+    hash = "sha256-3vDG0Yw0OUdwjZiRfwULLVhqTI8HrJcSooW0eyQup9g=";
+  };
+
+  nativeBuildInputs = [ binutils libarchive ];
+  buildInputs = [
+    gtk3
+    nss
+    libnotify
+    libXtst
+    xdg-utils
+    at-spi2-core
+    libdrm
+    mesa
+    libxcb
+    alsa-lib
+    libgcc
+    glib
+    autoPatchelfHook
+  ];
+
+  dontAutoPatchelf = false;
+
+  unpackPhase = ''
+    # Extract the .deb file using `ar`
+    ar x $src
+    bsdtar -xf data.tar.xz 
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin $out/share
+    cp -r usr/* $out/
+  '';
+
 }
