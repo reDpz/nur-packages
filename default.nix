88e78ee7449404fe16f523f2c966fff263e68d7e
reDpz <sv_redpz@outlook.com>
2025-01-30 19:52:48 +0000

    change name

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  frame0 = pkgs.callPackage ./pkgs/frame0 { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
